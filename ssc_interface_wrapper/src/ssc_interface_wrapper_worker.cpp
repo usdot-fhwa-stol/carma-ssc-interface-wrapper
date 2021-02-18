fb1a738677df9219c31ce7a178c383020f3d7053
Isaiah <isaiah.d.lewis@leidos.com>
2021-02-18 13:39:07 +0000

    Commit edits

--- ssc_interface_wrapper/src/ssc_interface_wrapper_worker.cpp
+++ ssc_interface_wrapper/src/ssc_interface_wrapper_worker.cpp
@@ -19,7 +19,6 @@
 
 uint8_t SSCInterfaceWrapperWorker::get_driver_status(const ros::Time& current_time, double timeout)
 {
-	ROS_DEBUG_STREAM("TEST_NODE1");
 
     if(last_vehicle_status_time_.isZero() || latest_ssc_status_.compare("not_ready") == 0)
     {
@@ -27,7 +26,6 @@ uint8_t SSCInterfaceWrapperWorker::get_driver_status(const ros::Time& current_ti
     }
     else if(current_time - last_vehicle_status_time_ > ros::Duration(timeout) || latest_ssc_status_.compare("fatal") == 0 
 	|| latest_ssc_status_.compare("failure") == 0) {
-		ROS_DEBUG_STREAM("TEST_NODE2");
         return cav_msgs::DriverStatus::FAULT;
     }
     return cav_msgs::DriverStatus::OPERATIONAL;
@@ -68,7 +66,6 @@ int SSCInterfaceWrapperWorker::convert_shift_state_to_J2735(const pacmod_msgs::S
 
 bool SSCInterfaceWrapperWorker::is_engaged()
 {
-	ROS_DEBUG_STREAM("TEST_NODE_");
 
     return !latest_ssc_status_.compare("active");
 }
