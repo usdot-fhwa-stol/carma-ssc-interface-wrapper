1cf113c18d29ec06d74ac9e4c316e19ca583956a
Kyle Rush <kyle.rush@leidos.com>
2022-04-11 08:26:30 -0400

    Updating version numbers for CARMA release

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
-FROM usdotfhwastolcandidate/autoware.ai:jetta-gillet AS base_image
+FROM usdotfhwastol/autoware.ai:carma-system-4.0.0 AS base_image
 
 FROM base_image as source-code
 
--- docker/checkout.bash
+++ docker/checkout.bash
@@ -54,8 +54,8 @@ if [[ "$BRANCH" = "develop" ]]; then
       git clone https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}/src/CARMAMsgs --branch $BRANCH
       git clone https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}/src/CARMAUtils --branch $BRANCH
 else
-      git clone https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}/src/CARMAMsgs --branch develop
-      git clone https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}/src/CARMAUtils --branch develop
+      git clone https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}/src/CARMAMsgs --branch carma-system-4.0.0
+      git clone https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}/src/CARMAUtils --branch carma-system-4.0.0
 fi
 
 if [ $build_ros1_pkgs -eq 1 ]; then
