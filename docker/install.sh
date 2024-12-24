b414252f9c0ea7d1c3e0406f1a0e228fbfab181c
Misheel Bayartsengel <misheel.bayart@gmail.com>
2024-12-24 07:07:47 -0500

    fix install paths

--- docker/install.sh
+++ docker/install.sh
@@ -65,7 +65,7 @@ if [ $build_ros1_pkgs -eq 1 ]; then
     cd ~/workspace_ros1
     echo "ROS1 build"
     source /home/carma/catkin/setup.bash
-    source /opt/autoware.ai/ros/install/setup.bash
+    #source /opt/autoware.ai/ros/install/setup.bash
     sudo apt-get install -y apt-utils
 
     rosdep install --from-paths src --ignore-src -r -y
@@ -89,11 +89,11 @@ if [ $build_ros1_pkgs -eq 1 ]; then
 elif [ $build_ros2_pkgs -eq 1 ]; then
 
     cd ~/workspace_ros2
-    source /opt/autoware.ai/ros/install_ros2/setup.bash
+    source /opt/autoware.ai/ros/install/setup.bash
     sudo apt-get update
     sudo apt-get install -y apt-utils
 
-    colcon build --packages-up-to ssc_interface_wrapper_ros2 pacmod3 kvaser_interface --build-base ./build_ssc_interface_wrapper --install-base /opt/carma/install_ros2 --cmake-args -DCMAKE_BUILD_TYPE=Release
+    colcon build --packages-up-to ssc_interface_wrapper_ros2 pacmod3 kvaser_interface --build-base ./build_ssc_interface_wrapper --install-base /opt/carma/install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
     # Get the exit code from the ROS2 build
     status=$?
@@ -107,7 +107,7 @@ elif [ $build_ros2_pkgs -eq 1 ]; then
 fi
 
 cd ~
-source /opt/autoware.ai/ros/install_ros2/setup.bash
+source /opt/autoware.ai/ros/install/setup.bash
 sudo apt-get update
 sudo apt-get -qq install apt-transport-s3
 
@@ -120,4 +120,4 @@ sudo sh -c 'echo "deb [trusted=yes] s3://autonomoustuff-ssc $(lsb_release -sc) m
 sudo apt list | grep "autonomoustuff-ssc"
 sudo sh -c 'echo "deb [trusted=yes] https://s3.amazonaws.com/autonomoustuff-repo/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/autonomoustuff-public.list'
 sudo apt-get update
-sudo apt-get -y install ros-humble-ssc-pm-gem && exit 0 || echo "Installation failed for ssc_pm_lexus check access_key and secret_id" && exit 1
+sudo apt-get -y install ros-humble-ssc-* && exit 0 || echo "Installation failed for ssc_pm_lexus check access_key and secret_id" && exit 1
