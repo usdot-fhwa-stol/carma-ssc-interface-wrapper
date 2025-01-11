180bbd8994999de8f8ade3beb131a2eb086ca9d2
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-01-11 01:25:03 -0500

    Fix git checkout for ros1

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -53,7 +53,13 @@ done
 cd ${dir}
 git clone --depth 1 https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}/src/CARMAMsgs --branch $BRANCH
 git clone --depth 1 https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}/src/CARMAUtils --branch $BRANCH
-git clone --depth 1 --sparse https://github.com/usdot-fhwa-stol/autoware.ai ${dir}/src/autoware.ai --branch $BRANCH
+
+if [[ "$BRANCH" == "develop" || "$BRANCH" == "master"]]; then
+      git clone --depth 1 --sparse https://github.com/usdot-fhwa-stol/autoware.ai ${dir}/src/autoware.ai --branch carma-$BRANCH
+else
+      git clone --depth 1 --sparse https://github.com/usdot-fhwa-stol/autoware.ai ${dir}/src/autoware.ai --branch $BRANCH
+fi
+
 cd ${dir}/src/autoware.ai
 git sparse-checkout set messages/autoware_msgs jsk_recognition/jsk_recognition_msgs
 
